a63b35e6c0b1a7c50ddc75f01cc75d3d8509b65f
Chris Chen <chris@openendings.net>
2026-04-18 12:02:44 +1000

    Separation results

--- Mathlib/Order/Continuous/Basic.lean
+++ Mathlib/Order/Continuous/Basic.lean
@@ -51,19 +51,31 @@ theorem compact_element_wayBelow_iff {α : Type u} [PartialOrder α] {x : α}
 proof_wanted wayBelowBasis_directed {α : Type u}
     [PartialOrder α] (b : Set α) (hb : IsWayBelowBasis b) : DirectedOn (· ≤ ·) b
 
--- TODO basis separation
-
-
-proof_wanted le_dirSupOfWayBelow_separation {α : Type u} [PartialOrder α]
-    (b : Set α) {{x y : α}} (hxy : ¬x ≤ y) (hy : IsDirSupOfWayBelow y b) :
-    ∃ z ∈ b, IsWayBelow z x ∧ ¬z ≤ y
+theorem le_dirSupOfWayBelow_separation {α : Type u} [PartialOrder α]
+    (b : Set α) {{x y : α}} (hx : IsDirSupOfWayBelow x b) :
+    (x ≤ y) ↔
+    ∀ z ∈ b, IsWayBelow z x → z ≤ y := by
+  apply Iff.intro
+  case mp =>
+    intros h z hz hzx
+    exact hzx.le.trans h
+  case mpr =>
+    intro h
+    rcases hx with ⟨h_ne, h_dir, h_sup⟩
+    -- TODO : weaken hx by h_dir
+    apply ((isLUB_iff_le_iff.mp h_sup) y).mpr _
+    apply mem_upperBounds.mpr
+    simp only [Set.mem_setOf_eq, and_imp]
+    exact h
 
 /--
 `¬x ≤ y` is witnessed by a basis element.
 -/
-proof_wanted wayBelowBasis_separation_not_le {α : Type u} [PartialOrder α]
-    (b : Set α) {{x y : α}} (hxy : ¬x ≤ y) (hb : IsWayBelowBasis b) :
-    ∃ z ∈ b, IsWayBelow z x ∧ ¬z ≤ y
+theorem wayBelowBasis_separation_not_le {α : Type u} [PartialOrder α]
+    (b : Set α) {{x y : α}} (hb : IsWayBelowBasis b)
+    (hxy : ∀ z ∈ b, IsWayBelow z x → z ≤ y) :
+    x ≤ y := by
+  exact (le_dirSupOfWayBelow_separation b (hb x)).mpr hxy
 
 -- TODO basis interpolation
 
