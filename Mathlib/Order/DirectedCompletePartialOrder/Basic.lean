52bd9aa22016dd8d68279eee6750c2ba894e8c04
Chris Chen <chris@openendings.net>
2026-04-22 19:09:03 +1000

    Stub DirectedCompletePartialOrder.Basic

---
+++ Mathlib/Order/DirectedCompletePartialOrder/Basic.lean
@@ -0,0 +1,25 @@
+/-
+Copyright (c) 2026 Chris Chen. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Chris Chen
+-/
+module
+
+public import Mathlib.Order.Bounds.Basic
+public import Mathlib.Order.DirectedCompletePartialOrder.Defs
+public import Mathlib.Order.ScottContinuity
+
+/-!
+# Basic results on directed complete partial orders
+
+This file contains some basic results on directed complete partial orders
+and their interaction with `ScottContinuous` functions.
+
+Definitions are in `Mathlib/Order/DirectedCompletePartialOrder/Defs.lean`.
+-/
+
+@[expose] public section
+
+variable {ι : Sort*} {α : Type*} [DirectedCompletePartialOrder α]
+
+end
